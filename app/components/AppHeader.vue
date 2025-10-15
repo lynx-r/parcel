21fee8c831e38ac7a4f582fee2904886f5fc6c51
Aleksey P <aleksey.p10n@gmail.com>
2025-10-15 12:45:35 +0300

    fix: fix header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,8 +1,12 @@
 <template>
   <UHeader title="Parcel">
-    <h1 class="text-2xl font-bold text-gray-900">
-      FSM Delivery Testing Interface
-    </h1>
-    <p class="text-sm text-gray-600">Parcel Locker Delivery Service Emulator</p>
+    <div class="flex flex-col">
+      <h1 class="text-2xl font-bold text-gray-900">
+        FSM Delivery Testing Interface
+      </h1>
+      <p class="text-sm text-gray-600">
+        Parcel Locker Delivery Service Emulator
+      </p>
+    </div>
   </UHeader>
 </template>
