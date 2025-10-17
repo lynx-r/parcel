c42330d9e5a2cbe53ea10799a1fb79a6d7ec70c3
Aleksey P <aleksey.p10n@gmail.com>
2025-10-17 15:49:48 +0300

    fix: different client

--- server/utils/prisma.server.ts
+++ server/utils/prisma.server.ts
@@ -1,6 +1,6 @@
 // import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaClient } from '~~/prisma/generated/client'
+import { PrismaClient } from '~~/prisma/generated/client/client'
 // import { PrismaClient } from '../../prisma/generated/client'
 
 // const { PrismaClient } = pkg
